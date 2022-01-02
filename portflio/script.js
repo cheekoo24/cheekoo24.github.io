c6befb27f381d413f39f177acd433d04b8a38cd1
Herman Redona <herman_lebrilla@yahoo.com>
2022-01-02 14:35:03 -0500

    new folder portfolio

--- portflio/script.js
+++ portflio/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500) {
+        if(this.scrollY > 20) {
             $('.scroll-up-btn').addClass("show");
         } else {
             $('.scroll-up-btn').removeClass("show");
